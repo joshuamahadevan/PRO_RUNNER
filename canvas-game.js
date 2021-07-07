53dd5237c49ae10dfd554ec3005d0e12d0df5693
joshuamahadevan <joshuamahadevan23@gmail.com>
2021-07-07 22:20:30 +0530

    added a moving obstacle

--- canvas-game.js
+++ canvas-game.js
@@ -41,13 +41,15 @@ class Player{
         this.y=DOWN-size;
         this.angle=0;
         this.transition=15;
+        this.cx=0;
+        this.cy=0;
     }
     draw() {
         this.update();
-        c.fillStyle= "#FF4F58FF";
         c.save();
         c.translate(this.x,this.y);
         c.rotate(this.angle);
+        c.fillStyle= "#FF4F58FF";
         
         if(this.pos=="d"){
             c.beginPath();
@@ -63,7 +65,6 @@ class Player{
             c.lineTo(this.size/2,-this.size)
             c.fill();
         }
-        console.log(this.angle)
         c.restore();
     }
     update(){
@@ -87,6 +88,9 @@ class Player{
             this.x=Math.floor(secWidth/2);
             this.angle=0;
         }
+
+        this.cx=this.x+this.size*(Math.cos(this.angle)/2+Math.sin(this.angle)/3);
+        this.cy=this.y-this.size*(Math.cos(this.angle)/3-Math.sin(this.angle)/2);
     }
 }
 
@@ -97,7 +101,7 @@ class Hole{
         this.pos=pos;
     }
     draw(){   
-        c.fillStyle="rgba(240, 246, 247, .95)";    
+        c.fillStyle="rgba(240, 246, 247, .9)";    
         if(this.pos==="u"){
             c.fillRect(this.start,0,this.width,UP);
         }
@@ -118,7 +122,6 @@ class Hole{
                 flag+=1;
             }
             if(flag>1){
-                terminate();
                 return true;
             }else{
                 return false;
@@ -135,11 +138,51 @@ class Hole{
                 flag+=1;
             }
             if(flag>1){
-                terminate();
                 return true;
             }else{
                 return false;
             }
+        }
+    }
+}
+
+class MovingObstacle{
+    constructor (start,width){
+        this.start=start;
+        this.width=width;
+        this.y=Math.floor(UP+this.width/2);
+        this.direction="d";
+    }
+    draw(){
+        this.update();
+        c.fillStyle="red";
+        c.beginPath();
+        c.arc(this.start+this.width/2,this.y,this.width/4,0,Math.PI*2);
+        c.fill();
+    }
+    update(){
+        let dy =Math.floor((DOWN-UP)/(60-score/50));
+        if(this.direction=="d"){
+            this.y+=dy;
+        }else if(this.direction=="u"){
+            this.y-=dy;
+        }
+        let radius=this.width/4;
+        if(this.y<UP+radius){
+            this.y=UP+radius;
+            this.direction="d";
+        }else if(this.y > DOWN - radius){
+            this.y=DOWN-radius;
+            this.direction="u";
+        }
+    }
+    checkCollision(){
+        //because the hit boxes are circles, there can be small imperfections in collision detection as the player object is triangular shaped. but it works good enough when i tried and i decided to leave it that way 
+        if ( Math.sqrt(Math.pow(this.start+this.width/2-player.cx,2)+Math.pow(this.y-player.cy,2)) < this.width/4+player.size/2.5){ 
+            return true;
+        }
+        else{
+            return false;
         }
     }
 }
@@ -160,7 +203,7 @@ class Empty{
 }
 //game controls - event listeners
 
-addEventListener("click", ()=>{
+addEventListener("mousedown", ()=>{
     if(player.pos=="d"){
         player.pos="u";
     }else if(player.pos=="u"){
@@ -189,7 +232,6 @@ addEventListener("keydown", function asdf (ev) {
 addEventListener("resize",resize);
 
 document.querySelector("button").addEventListener("click", () =>{
-    console.log("refreshing");
     location.reload();
 })
 
@@ -202,7 +244,7 @@ addEventListener("load", () => {
 //functions for the game
 
 function drawScene(){
-    c.fillStyle="rgba(240, 246, 247, .95)";
+    c.fillStyle="rgba(240, 246, 247, .9)";
     c.fillRect(0,UP,innerWidth,DOWN-UP);
     c.fillStyle="#669DB3FF";
     c.fillRect(0,0,innerWidth,UP);
@@ -216,6 +258,7 @@ function drawElements(){
 function checkCollisions(){
     for (let i=0; i<elements.length; i++){
         if(elements[i].checkCollision()){
+            terminate();
             break;
         }
     }
@@ -227,9 +270,12 @@ function randomElement(){
         return new Hole(0,0,"d");
     }else if(x>.85){
         return new Hole(0,0,"u");
+    }else if( .45<x && x<.55){
+        return new MovingObstacle(0,0);
     }else {
         return new Empty(0,0);
     }
+    
 }
 function terminate(){
     cancelAnimationFrame(reqId);
@@ -239,7 +285,6 @@ function terminate(){
     document.querySelector("h1").innerHTML=score;
     if(localStorage.getItem("highscore")==null){
         localStorage.setItem("highscore", score);
-        console.log("updated highscore");
     }
     else if(localStorage.getItem("highscore") < score){
         localStorage.setItem("highscore", score)
@@ -282,7 +327,6 @@ function play(){
     }
    
     //drawing elements on the canvas
-    c.clearRect(0,0,canvas.width,canvas.height)
     drawScene();
     drawElements();
     player.draw();
@@ -301,6 +345,4 @@ function play(){
     }
 
 }
-
-play()
-
+play();
